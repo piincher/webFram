d7cd8ef17509cfc81c9cc7a6d509ace32e2b049b
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-08 07:16:31 +0800

    interface optional

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 22 });
 
+user.set({ name: 'piin', age: 93 });
 console.log(user.get('name'));
 console.log(user.get('age'));
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,15 @@
 interface UserProps {
-	name: string;
-	age: number;
+	name?: string;
+	age?: number;
 }
 export class User {
 	constructor(private data: UserProps) {}
 
 	get(propsName: string): number | string {
 		return this.data[propsName];
+	}
+
+	set(update: UserProps): void {
+		Object.assign(this.data, update);
 	}
 }
