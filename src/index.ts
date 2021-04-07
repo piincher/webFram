235a4755dfd64cc3d3fe2ae77d1836cdb556c204
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-08 06:56:26 +0800

    update the index file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log('hi ther ');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 22 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
