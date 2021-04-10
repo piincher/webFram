76553e4f576695413d92b935956e0f7746f29b79
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-10 12:44:16 +0800

    trigger event callback

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,11 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 22 });
 
-user.on('change', () => {});
-console.log(user);
+user.on('change', () => {
+	console.log('change 1');
+});
+user.on('change', () => {
+	console.log('change2');
+});
+
+user.trigger('change');
