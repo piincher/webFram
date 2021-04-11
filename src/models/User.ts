f5a1dc5f3af1c63f6eb05ef69b1d5627098f4723
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-12 06:33:50 +0800

    refactor with composition

--- src/models/User.ts
+++ src/models/User.ts
@@ -5,9 +5,7 @@ interface UserProps {
 	age?: number;
 }
 
-type CallBack = () => void;
 export class User {
-	events: { [key: string]: CallBack[] } = {};
 	constructor(private data: UserProps) {}
 
 	get(propsName: string): number | string {
@@ -18,22 +16,6 @@ export class User {
 		Object.assign(this.data, update);
 	}
 
-	on(eventName: string, callback: CallBack): void {
-		const handlers = this.events[eventName] || [];
-		handlers.push(callback);
-		this.events[eventName] = handlers;
-	}
-
-	trigger(eventName: string): void {
-		const handlers = this.events[eventName];
-
-		if (!handlers || handlers.length === 0) {
-			return;
-		}
-		handlers.forEach((callBack) => {
-			callBack();
-		});
-	}
 	fetch(): void {
 		axios.get(`http://localhost:3000/users/${this.get('id')}`).then((response: AxiosResponse): void => {
 			this.set(response.data);
