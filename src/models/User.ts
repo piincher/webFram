a9d10ab40ecf1f7c9669b9a649b1d382697544c3
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-13 22:31:34 +0800

    remove fetch and save method

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,3 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 interface UserProps {
 	id?: number;
@@ -16,21 +15,5 @@ export class User {
 
 	set(update: UserProps): void {
 		Object.assign(this.data, update);
-	}
-
-	fetch(): void {
-		axios.get(`http://localhost:3000/users/${this.get('id')}`).then((response: AxiosResponse): void => {
-			this.set(response.data);
-		});
-	}
-
-	save(): void {
-		const id = this.get('id');
-		if (id) {
-			//put
-			axios.put(`http://localhost:3000/users/${id}`, this.data);
-		} else {
-			axios.post('http://localhost:3000/users', this.data);
-		}
 	}
 }
