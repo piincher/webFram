8ed5d81f4a0dbc639b6be57a246d2bbb19fbebb0
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-17 11:34:32 +0800

    composition is delegationn

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,2 @@
 import { User } from './models/User';
 const user = new User({ name: 'bintou', age: 232 });
-
-user.save();
-user.events.on('change', () => {
-	console.log('change!');
-});
-user.events.trigger('change');
