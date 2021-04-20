02ca8f016a094175aab5edd069d7252750c8d173
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-20 11:28:43 +0800

    create a static method

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
-import { AxiosResponse } from 'axios';
-import { Eventing } from './Eventing';
-import { Sync } from './Sync';
+import { Model } from './Model';
 import { Attributes } from './Attribut';
+import { Eventing } from './Eventing';
+import { ApiSync } from './ApiSync';
 export interface UserProps {
 	id?: number;
 	name?: string;
@@ -9,42 +9,8 @@ export interface UserProps {
 }
 const rootUrl = 'http://localhost:3000/users';
 
-export class User {
-	public events: Eventing = new Eventing();
-	public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-	public attributes: Attributes<UserProps>;
-
-	constructor(attrs: UserProps) {
-		this.attributes = new Attributes<UserProps>(attrs);
-	}
-
-	get on() {
-		return this.events.on;
-	}
-	get trigger() {
-		return this.events.trigger;
-	}
-
-	get get() {
-		return this.attributes.get;
-	}
-
-	set(update: UserProps): void {
-		this.attributes.set(update);
-		this.events.trigger('change');
-	}
-
-	async fetch(): Promise<void> {
-		const id = this.get('id');
-		if (typeof id !== 'number') {
-			throw new Error('cannot without id ');
-		}
-		const response: AxiosResponse = await this.sync.fetch(id);
-		this.set(response.data);
-	}
-
-	async save(): Promise<void> {
-		const { data: AxiosResponse } = await this.sync.save(this.attributes.getAll());
-		this.trigger('save');
+export class User extends Model<UserProps> {
+	static buildUser(attrs: UserProps): User {
+		return new User(new Attributes<UserProps>(attrs), new Eventing(), new ApiSync<UserProps>(rootUrl));
 	}
 }
