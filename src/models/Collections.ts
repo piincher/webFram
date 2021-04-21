c0f3c3b85aa0294fd4dc6b31450864848a36dae1
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-21 12:05:35 +0800

    implement collections of users

---
+++ src/models/Collections.ts
@@ -0,0 +1,14 @@
+import { User } from './User';
+import { Eventing } from './Eventing';
+export class Collection {
+	models: User[] = [];
+	events: Eventing = new Eventing();
+
+	get on() {
+		return this.events.on;
+	}
+
+	get trigger() {
+		return this.events.trigger;
+	}
+}
