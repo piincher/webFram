60cd42b9930544475f7ba451f4298e7cbc5e130b
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-24 13:06:56 +0800

    fix a typo inside index.html and userForm

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -13,6 +13,6 @@ export class UserForm {
 	render(): void {
 		const templateElement = document.createElement('template');
 		templateElement.innerHTML = this.template();
-		this.parent.appendChild(templateElement);
+		this.parent.appendChild(templateElement.content);
 	}
 }
