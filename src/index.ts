7fc105d95efd2b2de0e09c15867a891dc889b1ca
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-24 12:58:35 +0800

    render HTML

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-import { User } from './models/User';
-const collection = User.buildUserCollection();
-collection.on('change', () => {
-	console.log(collection);
-});
-collection.fetch();
+import { UserForm } from './views/UserForm';
+
+const userForm = new UserForm(document.getElementById('root'));
+userForm.render();
