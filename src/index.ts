208faa5cca16d5f69acf95ab85ff0b171913effd
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-25 11:08:47 +0800

    adding models properties

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { UserForm } from './views/UserForm';
-
-const userForm = new UserForm(document.getElementById('root'));
+import { User } from './models/User';
+const user = User.buildUser({ name: 'sly', age: 20 });
+const userForm = new UserForm(document.getElementById('root'), user);
 userForm.render();
