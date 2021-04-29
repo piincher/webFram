c398e580149df03c6fc99ae2660f85091e728c93
Ibrahim Kouma <ikouma269@gmail.com>
2021-04-29 13:16:23 +0800

    save functionnality

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,9 +4,13 @@ export class UserForm extends View<User, UserProps> {
 	eventsMap(): { [key: string]: () => void } {
 		return {
 			'click:.set-age': this.onSetAgeClick,
-			'click:.set-name': this.onSetNameClick
+			'click:.set-name': this.onSetNameClick,
+			'click:.save-model': this.onSaveClick
 		};
 	}
+	onSaveClick = (): void => {
+		this.model.save();
+	};
 	onSetNameClick = (): void => {
 		const input = this.parent.querySelector('input');
 		if (input) {
@@ -21,12 +25,10 @@ export class UserForm extends View<User, UserProps> {
 	template(): string {
 		return `
         <div>
-        <h1>form</h1>
-		<div>User name : ${this.model.get('name')}</div>
-		<div> Age :${this.model.get('age')}</div>
-        <input/>
+        <input placeholder="${this.model.get('name')}"/>
 		<button class="set-name">change name</button>
 		<button class="set-age"> set  random number </button>
+		<button class="save-model">Save User</button>
         </div>
         `;
 	}
