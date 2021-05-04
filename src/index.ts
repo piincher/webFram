44b46dc89b580c415fdef622901a51e4f1c01f43
Ibrahim Kouma <ikouma269@gmail.com>
2021-05-04 15:52:09 +0800

    testing the maping regions

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 const user = User.buildUser({ name: 'sly', age: 20 });
 const root = document.getElementById('root');
 if (root) {
-	const userForm = new UserForm(root, user);
-	userForm.render();
+	const userEdit = new UserEdit(root, user);
+	userEdit.render();
+	console.log(userEdit);
 } else {
 	throw new Error('root dont found ');
 }
