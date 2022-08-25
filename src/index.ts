e14dc42887b2eb931ff4609fdb88981ad813c657
Ibrahim <ikouma269@gmail.com>
2022-08-26 06:28:19 +0800

    dynamic array creation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import { User } from "./modals/User";
 
 const user = new User({ name: "piincher", age: 20 });
-
+user.on("change", () => {});
+console.log(user);
 user.set({ name: "storm", age: 199 });
 console.log(user.get("name"));
 console.log(user.get("age"));
--- src/modals/User.ts
+++ src/modals/User.ts
@@ -3,7 +3,10 @@ interface UserProps {
   age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -12,5 +15,11 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
+  }
+  on(eventName: string, callBack: Callback) {
+    const handlers = this.events[eventName] || [];
+
+    handlers.push(callBack);
+    this.events[eventName] = handlers;
   }
 }
