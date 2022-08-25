4832dd33b0681a9cf130dff547c764e2882251ee
Ibrahim <ikouma269@gmail.com>
2022-08-26 06:14:08 +0800

    listener support

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import { User } from "./modals/User";
 
 const user = new User({ name: "piincher", age: 20 });
 
+user.set({ name: "storm", age: 199 });
 console.log(user.get("name"));
 console.log(user.get("age"));
